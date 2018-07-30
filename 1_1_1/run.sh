9771988450de1b6d80ec767343c2275fe1e6541a
wuhuo-org <31194713+wuhuo-org@users.noreply.github.com>
2018-07-30 17:24:58 +0800

    Add files via upload

--- 1_1_1/run.sh
+++ 1_1_1/run.sh
@@ -1,7 +1,7 @@
 as -o bootsect.o bootsect.s
 ld --oformat binary -Ttext=0 -o bootsect bootsect.o
-
 dd if=bootsect of=image bs=512
+
 cp ../bochs/raw.img ../bochs/c.img
 dd if=image of=..bochs/c.img bs=512 conv=notrunc
 
