34a674872d88de5ef2c58584f830a9cab0011e1f
wuhuo-org <31194713+wuhuo-org@users.noreply.github.com>
2018-07-30 15:47:33 +0800

    Update run.sh

--- 1_1_1/run.sh
+++ 1_1_1/run.sh
@@ -1,9 +1,6 @@
 as -o bootsect.o bootsect.s
-
 ld --oformat binary -Ttext=0 -o bootsect bootsect.o
-
 dd if=bootsect of=image bs=512
-
 cp image ../
-
-#rm -f *.o bootsect image
+rm -f *.o bootsect image
+../../bochs -q -f ../bochsrc.bxrc
