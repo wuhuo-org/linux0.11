5b74459ae488330fbbf839f758939312dd48a000
wuhuo-org <31194713+wuhuo-org@users.noreply.github.com>
2018-07-30 16:59:10 +0800

    Add files via upload

--- 1_1_2/run.sh
+++ 1_1_2/run.sh
@@ -7,6 +7,9 @@ ld --oformat binary -Ttext=0 -o setup setup.o
 dd if=bootsect of=image bs=512
 dd if=setup of=image bs=512 seek=1
 
-cp image ../
+cp ../bochs/raw.img ../bochs/c.img
+dd if=image of=..bochs/c.img bs=512 conv=notrunc
 
 rm -f *.o bootsect setup system image
+
+../../bochs -q -f ../bochs/bochsrc.bxrc
