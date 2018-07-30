57b3c8744c3e0fd1e98250bd801ed370adc5714e
wuhuo-org <31194713+wuhuo-org@users.noreply.github.com>
2018-07-30 17:51:38 +0800

    Update run.sh

--- 1_1_2/run.sh
+++ 1_1_2/run.sh
@@ -5,6 +5,6 @@ ld --oformat binary -Ttext=0 -o setup setup.o
 dd if=bootsect of=image bs=512
 dd if=setup of=image bs=512 seek=1
 cp ../bochs/raw.img ../bochs/c.img
-dd if=image of=..bochs/c.img bs=512 conv=notrunc
+dd if=image of=../bochs/c.img bs=512 conv=notrunc
 rm -f *.o bootsect setup system image
 ../../bochs -q -f ../bochs/bochsrc.bxrc
